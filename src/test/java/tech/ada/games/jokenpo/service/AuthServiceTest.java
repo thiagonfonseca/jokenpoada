93b4608ff40b7a74110eb30ead340ef5acc5c33a
julianafsa <juliana.aquino@f1rst.com.br>
2023-05-01 19:37:21 -0300

    Adding test class to AuthService class.

--- src/test/java/tech/ada/games/jokenpo/service/AuthServiceTest.java
+++ src/test/java/tech/ada/games/jokenpo/service/AuthServiceTest.java
@@ -1,4 +1,61 @@
-import static org.junit.jupiter.api.Assertions.*;
+package tech.ada.games.jokenpo.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import tech.ada.games.jokenpo.dto.LoginDto;
+import tech.ada.games.jokenpo.response.AuthResponse;
+import tech.ada.games.jokenpo.security.JwtTokenProvider;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
 class AuthServiceTest {
-  
+
+    private AuthenticationManager authenticationManager;
+    private JwtTokenProvider tokenProvider;
+    private AuthService service;
+
+    @BeforeEach
+    void setUp() {
+        this.authenticationManager = mock(AuthenticationManager.class);
+        this.tokenProvider = mock(JwtTokenProvider.class);
+        this.service = new AuthService(authenticationManager, tokenProvider);
+    }
+
+    @Test
+    void login() {
+        // Given (Arrange)
+        final LoginDto loginDto = this.buildLoginDto();
+        final Authentication usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+        final Authentication authentication = mock(Authentication.class);
+        final String token = this.buildToken();
+        when(authenticationManager.authenticate(usernamePasswordAuthenticationToken))
+                .thenReturn(authentication);
+        when(tokenProvider.generateToken(authentication))
+                .thenReturn(token);
+
+        // When (Act)
+        final AuthResponse response = service.login(loginDto);
+
+        // Then (Assert)
+        final AuthResponse expectedResponse = new AuthResponse(token);
+        assertEquals(expectedResponse.getAccessToken(), response.getAccessToken(), "Tokens should be equal");
+        verify(authenticationManager, times(1)).authenticate(usernamePasswordAuthenticationToken);
+        verify(tokenProvider, times(1)).generateToken(authentication);
+    }
+
+    private LoginDto buildLoginDto() {
+        final LoginDto loginDto = new LoginDto();
+        loginDto.setUsername("username");
+        loginDto.setPassword("password");
+        return loginDto;
+    }
+
+    private String buildToken() {
+        return "accessToken";
+    }
 }
